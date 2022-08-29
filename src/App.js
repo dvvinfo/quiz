55b66d1e890b4e638630445edd295deb31cdc93c
Vladimir Drobyshev <dvvinfo@mail.ru>
2022-08-29 23:03:52 +0300

    сделал quiz react приложение

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+import Game from "./components/Game";
+import Result from "./components/Result";
+
+import { questions } from "./questions";
+
+import "./index.scss";
+
+function App() {
+  const [step, setStep] = useState(0);
+  const [correct, setCorrect] = useState(0);
+  const question = questions[step];
+  const onClickVariant = (index) => {
+    setStep(step + 1);
+
+    if (index === question.correct) {
+      setCorrect(correct + 1);
+    }
+  };
+  return (
+    <div className="App">
+      {step !== questions.length ? (
+        <Game step={step} question={question} onClickVariant={onClickVariant} />
+      ) : (
+        <Result correct={correct} />
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Game.jsx
@@ -0,0 +1,25 @@
+import { questions } from "../questions";
+
+function Game({ step, question, onClickVariant }) {
+  const percentage = Math.round((step / questions.length) * 100);
+  return (
+    <>
+      <div className="progress">
+        <div
+          style={{ width: `${percentage}%` }}
+          className="progress__inner"
+        ></div>
+      </div>
+      <h1>{question.title}</h1>
+      <ul>
+        {question.variants.map((text, index) => (
+          <li key={text} onClick={() => onClickVariant(index)}>
+            {text}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default Game;
